a7f3209b218f9b8616ee456b4e0400ed4ba0fb64
ishitaraina1807 <ishitaraina18@gmail.com>
2023-12-02 19:00:23 +0530

    check

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,55 +1,44 @@
-'use client'
-import React, { useState } from 'react';
-// import searchIcon from './assests/search.png';
-// import bagIcon from './assests/bag.png';
-import logo from '../components/assests/logo.jpg';
+import { useState } from 'react';
 
-export default function NavBar() {
-  const [isOpen, setIsOpen] = useState(false);
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
   };
 
   return (
-    <div>
-      {/* Header */}
-      <div className='text-center text-xs lg:text-sm font-light text-[#f5efe3] bg-[#956f5a] uppercase tracking-wide py-1'>Welcome to our store!</div>
-      {/* Horizontal Rule (visible on medium and larger screens) */}
-      <hr className='border-1 border-[#C2A293] block' />
-      {/* Navbar */}
-      <div className='bg-[#956f5a] w-full md:h-56 flex flex-col md:flex-row items-center justify-center font-montserrat text-base font-normal leading-normal relative sm:pb-10 h-[100px]'>
-      <img className="lg:w-48 rounded-full w-28" src={logo} alt="logo" />
-        <div className={`w-full lg:w-1/2 text-[#f5efe3] lg:flex lg:justify-around lg:items-center lg:space-x-4 absolute top-full left-0 right-0 bg-[#956f5a] lg:bg-transparent z-50 ${isOpen ? 'flex flex-col' : 'hidden'} lg:relative lg:flex-row lg:visible lg:top-0 w-full lg:z-0`}>
-            <p className='py-2 lg:py-0 px-4 lg:px-0 text-center lg:text-left text-lg  hover:underline hover:cursor-pointer'>Home</p>
-            <p className='py-2 lg:py-0 px-4 lg:px-0 text-center lg:text-left text-lg  hover:underline hover:cursor-pointer'>Shop TheKraftCloset</p>
-            <p className='py-2 lg:py-0 px-4 lg:px-0 text-center lg:text-left text-lg  hover:underline hover:cursor-pointer'>FAQs</p>
-            <p className='py-2 lg:py-0 px-4 lg:px-0 text-center lg:text-left text-lg  hover:underline hover:cursor-pointer'>About us</p>
-            <p className='py-2 lg:py-0 px-4 lg:px-0 text-center lg:text-left text-lg  hover:underline hover:cursor-pointer'>Contact Us</p>
-          </div>
-          {/* section 3 */}
-          {/* <div className='w-8 lg:w-6 flex md:justify-end gap-6 lg:pt-0 pt-6 justify-center mt-48 -mr-20'>
-          <img src={searchIcon} alt="search" />
-          <img src={bagIcon} alt="bag" /> */}
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          {/* <Brand /> */}
         </div>
-        {/* Mobile Menu Toggle */}
-        <div className='lg:hidden absolute top-3 right-3 text-[#f5efe3]' onClick={toggleMenu}>
-          <svg
-            className='w-6 h-6 cursor-pointer'
-            fill='none'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            strokeWidth='2'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-          >
-            {isOpen ? (
-              <path d='M6 18L18 6M6 6l12 12' />
-            ) : (
-              <path d='M4 6h16M4 12h16m-7 6h7' />
-            )}
-          </svg>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          {/* <Hamburger /> */}
+          {showNavbar ? 'Close Menu' : 'Open Menu'}
+        </div>
+        <div className={`nav-elements ${showNavbar && 'active'}`}>
+          <ul>
+            <li>
+              <p>Home</p>
+            </li>
+            <li>
+              <p>Blog</p>
+            </li>
+            <li>
+              <p>Projects</p>
+            </li>
+            <li>
+              <p>About</p>
+            </li>
+            <li>
+              <p>Contact</p>
+            </li>
+          </ul>
         </div>
       </div>
+    </nav>
   );
-}
+};
+
+export default Navbar;
